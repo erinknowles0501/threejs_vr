87391b3ef3b54758b052a267665b10629d8240dc
Erin Knowles <erinknowles@protonmail.com>
2022-01-10 02:49:41 -0500

    Remove bug.

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ scene.add(cube);
 camera.position.z = 5;
 
 renderer.setAnimationLoop(function () {
-    requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
 
